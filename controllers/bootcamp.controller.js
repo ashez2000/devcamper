0623169350134b6737b70aad61a0e526c3753d45
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2020-12-03 10:41:14 +0530

    basic bootcamp CRUD

--- controllers/bootcamp.controller.js
+++ controllers/bootcamp.controller.js
@@ -1,19 +1,41 @@
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({success: true, message: "bootcamps"}) 
-}
+const asyncHandler = require('../middlewares/async.middleware')
+const Bootcamp = require('../models/bootcamp.model')
 
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({success: true, message: "bootcamp"}) 
-}
+// desc  : gets all bootcamps
+// route : GET /api/bootcamps | public
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    const bootcamps = await Bootcamp.find()
+    res.status(200).json({success: true, data: bootcamps}) 
+})
 
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({success: true, message: "bootcamp created"}) 
-}
+// desc  : gets a single bootcamp
+// route : GET /api/bootcamps/:id | public
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findById(req.params.id)
+    res.status(200).json({success: true, data: bootcamp}) 
+})
 
-exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({success: true, message: "bootcamp  updated"}) 
-}
+// desc  : creates a bootcamp
+// route : POST /api/bootcamps | private
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.create(req.body)
+    res.status(200).json({success: true, data: bootcamp}) 
+})
 
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({success: true, message: "bootcamp deleted"}) 
-}
+// desc  : updates a bootcamp
+// route : PUT /api/bootcamps/:id | private
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+    const opt = {
+        new: true,
+        runValidators: true
+    }
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, opt)
+    res.status(200).json({success: true, data: bootcamp}) 
+})
+
+// desc  : gets all bootcamps
+// route : DELETE /api/bootcamps | private
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+    await Bootcamp.findByIdAndDelete(req.params.id)
+    res.status(200).json({success: true, data: {}}) 
+})
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const dotenv = require('dotenv')
 const express = require('express')
 
+const connectDb = require('./db')
+
 const bootcamps = require('./routers/bootcamp.router')
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+connectDb()
+
+app.use(express.json())
+
+// routes
 app.use('/api/bootcamps', bootcamps)
 
-app.listen(PORT, console.log(`Server Live | PORT: ${PORT}`))
+const server = app.listen(PORT, console.log(`Server Live | PORT: ${PORT}`))
