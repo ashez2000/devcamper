2f3cc4c564d263addda0e95b58cf46fd2597d2c6
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-08-09 23:23:05 +0530

    add course service

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -1,106 +1,68 @@
-const Course = require('../models/course.model')
-const Bootcamp = require('../models/bootcamp.model')
 const asyncHandler = require('../middlewares/async.middleware')
-const ErrorResponse = require('../utils/error.util')
+const coureService = require('../services/courses.service')
 
 /**
  * @desc  Get all courses associated with a bootcamp
- * @route GET /api/v1/courses
  * @route GET /api/v1/bootcamps/:bootcampId/courses
+ * @access Public
  */
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  const courses = await Course.find({ bootcamp: req.params.bootcampId })
+  const courses = await coureService.find(req.params.bootcampId)
 
   res.status(200).json({
-    status: 'success',
+    results: courses.length,
     courses,
   })
 })
 
 /**
- * @desc   Get a course by id
- * @route  GET /api/v1/courses/:id
- * @access Private
+ * @desc  Get single course
+ * @route GET /api/v1/bootcamps/:bootcampId/courses/:id
+ * @access Public
  */
 exports.getCourse = asyncHandler(async (req, res, next) => {
-  const course = await Course.findById(req.params.id)
-
-  if (!course) {
-    return next(
-      new ErrorResponse(`Course not found with id ${req.params.id}`, 404)
-    )
-  }
+  const course = await coureService.findOne(req.params.id)
 
   res.status(200).json({
-    status: 'success',
     course,
   })
 })
 
 /**
- * @desc   Create a course
- * @route  POST /api/v1/bootcamps/:bootcampId/courses
+ * @desc  Create a new course
+ * @route POST /api/v1/bootcamps/:bootcampId/courses
  * @access Private
  */
 exports.createCourse = asyncHandler(async (req, res, next) => {
-  req.body.bootcamp = req.params.bootcampId
-  const bootcamp = await Bootcamp.findById(req.params.bootcampId)
-
-  if (!bootcamp) {
-    return next(
-      new ErrorResponse(
-        `Bootcamp not found with id ${req.params.bootcampId}`,
-        404
-      )
-    )
-  }
-
-  const course = await Course.create(req.body)
+  const course = await coureService.create(req.body)
 
   res.status(201).json({
-    status: 'success',
     course,
   })
 })
 
 /**
- * @desc   Update a course
- * @route  PUT /api/v1/courses/:id
+ * @desc  Update a course
+ * @route PUT /api/v1/bootcamps/:bootcampId/courses/:id
  * @access Private
  */
 exports.updateCourse = asyncHandler(async (req, res, next) => {
-  const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  })
-
-  if (!course) {
-    return next(
-      new ErrorResponse(`Course not found with id ${req.params.id}`, 404)
-    )
-  }
+  const course = await coureService.update(req.params.id, req.body)
 
   res.status(200).json({
-    status: 'success',
     course,
   })
 })
 
 /**
- * @desc   Delete a course
- * @route  DELETE /api/v1/courses/:id
+ * @desc  Delete a course
+ * @route DELETE /api/v1/bootcamps/:bootcampId/courses/:id
  * @access Private
  */
 exports.deleteCourse = asyncHandler(async (req, res, next) => {
-  const course = await Course.findByIdAndDelete(req.params.id)
-
-  if (!course) {
-    return next(
-      new ErrorResponse(`Course not found with id ${req.params.id}`, 404)
-    )
-  }
+  const course = await coureService.delete(req.params.id)
 
   res.status(200).json({
-    status: 'success',
     course,
   })
 })
--- src/routers/course.router.js
+++ src/routers/course.router.js
@@ -1,31 +1,14 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true })
 
-const {
-  getCourses,
-  getCourse,
-  createCourse,
-  updateCourse,
-  deleteCourse,
-} = require('../controllers/course.controller')
+const courseCtrl = require('../controllers/course.controller')
 
-// advanced results
-const Course = require('../models/course.model')
-const advRes = require('../middlewares/advResults.middleware')
-const populate = { path: 'bootcamp', select: 'name description' }
-
-// auth middleware
-const { protect, authorize } = require('../middlewares/auth.middleware')
-
-router
-  .route('/')
-  .get(advRes(Course, populate), getCourses)
-  .post(protect, authorize('admin', 'publisher'), createCourse)
+router.route('/').get(courseCtrl.getCourses).post(courseCtrl.createCourse)
 
 router
   .route('/:id')
-  .get(getCourse)
-  .put(protect, authorize('admin', 'publisher'), updateCourse)
-  .delete(protect, authorize('admin', 'publisher'), deleteCourse)
+  .get(courseCtrl.getCourse)
+  .put(courseCtrl.updateCourse)
+  .delete(courseCtrl.deleteCourse)
 
 module.exports = router
