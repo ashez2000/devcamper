b419056910f981bf4ff54919ba2505c6fb024a6d
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-10-04 20:57:23 +0530

    Move course and review files to their dirs

--- src/schemas/course.schema.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod'
-
-export const CourseSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  tuition: z.number(),
-})
-
-export type CourseInputData = z.infer<typeof CourseSchema>
--- src/schemas/review.schema.ts
+++
@@ -1,8 +0,0 @@
-import z from 'zod'
-
-export const ReviewSchema = z.object({
-  text: z.string(),
-  rating: z.number().min(1).max(10),
-})
-
-export type ReviewInputData = z.infer<typeof ReviewSchema>
