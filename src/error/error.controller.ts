a7d8ffcd6360e473fb0a7d4b4bf88a26fc2173cb
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-11-15 14:29:09 +0530

    fix: tsconfig and error controller

--- src/error/error.controller.ts
+++ src/error/error.controller.ts
@@ -1,31 +1,12 @@
 import { ErrorRequestHandler, RequestHandler } from 'express'
-import { Error as MongooseError } from 'mongoose'
 import AppError from '../utils/app-error'
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.log('Error Handler:', err.message)
-  console.log(err)
   console.log(err.stack)
 
-  if (err instanceof MongooseError.CastError) {
-    err = new AppError('Invalid resource id', 400)
-  }
-
-  if (err.code === 11000) {
-    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
-    const message = `Duplicate field value: ${value}. Please use another value!`
-
-    err = new AppError(message, 404)
-  }
-
-  if (err instanceof MongooseError.ValidationError) {
-    const message = Object.values(err.errors).map((val) => val.message)
-    console.log(message)
-    err = new AppError(message.join(', '), 404)
-  }
-
-  let message = err.message || 'Server Error'
-  let statusCode = err.statusCode || 500
+  const message = err.message || 'Server Error'
+  const statusCode = err.statusCode || 500
   return res.status(statusCode).json({ error: message })
 }
 
