37d6849cd4c8a5575e00096c649741d5dfa9aca1
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-06-10 19:40:08 +0530

    Add prima with minial bootcamp repo

---
+++ src/db/repo/bootcamp.repo.ts
@@ -0,0 +1,39 @@
+import { db } from '../conn'
+
+export async function findAll() {
+    const bootcamps = await db.bootcamp.findMany()
+    return bootcamps
+}
+
+export async function findById(id: string) {
+    const bootcamp = await db.bootcamp.findUnique({
+        where: { id },
+    })
+
+    return bootcamp
+}
+
+export async function create(data: any) {
+    const bootcamp = await db.bootcamp.create({
+        data,
+    })
+
+    return bootcamp
+}
+
+export async function update(id: string, data: any) {
+    const bootcamp = await db.bootcamp.update({
+        where: { id },
+        data,
+    })
+
+    return bootcamp
+}
+
+export async function remove(id: string) {
+    const bootcamp = await db.bootcamp.delete({
+        where: { id },
+    })
+
+    return bootcamp
+}
