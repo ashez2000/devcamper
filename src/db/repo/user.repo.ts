84b0a8ca8b7158165c908e106a0eda1989404d72
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-06-10 20:04:36 +0530

    Add base user repo

---
+++ src/db/repo/user.repo.ts
@@ -0,0 +1,23 @@
+import argon from 'argon2'
+import { db } from '../conn'
+
+export async function findById(id: string) {
+    const user = await db.user.findUnique({ where: { id } })
+    return user
+}
+
+export async function findByEmail(email: string) {
+    const user = await db.user.findUnique({ where: { email } })
+    return user
+}
+
+export async function create(data: any) {
+    const { name, email, password, role } = data
+    const hash = await argon.hash(password)
+
+    const user = await db.user.create({
+        data: { name, email, password: hash, role },
+    })
+
+    return user
+}
