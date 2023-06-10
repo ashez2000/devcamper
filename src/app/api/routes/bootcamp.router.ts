ccbb7117d28f996107d83156326bfffb3d4aa728
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-06-10 20:57:07 +0530

    Protect and authorize bootcamp and reviews

--- src/app/api/routes/bootcamp.router.ts
+++ src/app/api/routes/bootcamp.router.ts
@@ -1,13 +1,32 @@
 import { Router } from 'express'
+
+import { protect, authorize } from '../middlewares/auth.middleware'
 import * as bootcampCtrl from '../controllers/bootcamp.controller'
 
 const router = Router()
 
 router.get('/', bootcampCtrl.findAll)
-router.post('/', bootcampCtrl.create)
+router.post('/', protect, authorize('publisher', 'admin'), bootcampCtrl.create)
 
-router.get('/:id', bootcampCtrl.findById)
-router.put('/:id', bootcampCtrl.update)
-router.delete('/:id', bootcampCtrl.remove)
+router.get(
+    '/:id',
+    protect,
+    authorize('publisher', 'admin'),
+    bootcampCtrl.findById
+)
+
+router.put(
+    '/:id',
+    protect,
+    authorize('publisher', 'admin'),
+    bootcampCtrl.update
+)
+
+router.delete(
+    '/:id',
+    protect,
+    authorize('publisher', 'admin'),
+    bootcampCtrl.remove
+)
 
 export { router as bootcampRouter }
--- src/app/api/routes/review.router.ts
+++ src/app/api/routes/review.router.ts
@@ -1,12 +1,31 @@
 import { Router } from 'express'
+
+import { protect, authorize } from '../middlewares/auth.middleware'
 import * as reviewCtrl from '../controllers/review.controller'
 
 const router = Router()
 
 router.get('/bootcamps/:bootcampId/reviews', reviewCtrl.findAllByBootcampId)
-router.post('/bootcamps/:bootcampId/reviews', reviewCtrl.create)
 
-router.put('/reviews/:id', reviewCtrl.update)
-router.delete('/reviews/:id', reviewCtrl.remove)
+router.post(
+    '/bootcamps/:bootcampId/reviews',
+    protect,
+    authorize('publisher', 'admin'),
+    reviewCtrl.create
+)
+
+router.put(
+    '/reviews/:id',
+    protect,
+    authorize('user', 'admin'),
+    reviewCtrl.update
+)
+
+router.delete(
+    '/reviews/:id',
+    protect,
+    authorize('publisher', 'admin'),
+    reviewCtrl.remove
+)
 
 export { router as reviewRouter }
