b3c3786c7502bbc717081b621cf3c5ac052ffa93
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-06-13 20:10:55 +0530

    Add user types

--- src/db/repo/user.repo.ts
+++ src/db/repo/user.repo.ts
@@ -1,5 +1,7 @@
 import argon from 'argon2'
+
 import { db } from '../conn'
+import { CreateUserInput } from '../schema/user.schema'
 
 export async function findById(id: string) {
     const user = await db.user.findUnique({ where: { id } })
@@ -11,7 +13,7 @@ export async function findByEmail(email: string) {
     return user
 }
 
-export async function create(data: any) {
+export async function create(data: CreateUserInput) {
     const { name, email, password, role } = data
     const hash = await argon.hash(password)
 
--- src/db/schema/user.schema.ts
+++ src/db/schema/user.schema.ts
@@ -4,10 +4,12 @@ export const SignupSchema = z.object({
     name: z.string().min(3).max(50),
     email: z.string().email(),
     password: z.string().min(6).max(50),
-    role: z.enum(['user', 'publisher']),
+    role: z.enum(['user', 'publisher']).default('user'),
 })
 
 export const SigninSchema = z.object({
     email: z.string(),
     password: z.string(),
 })
+
+export type CreateUserInput = z.infer<typeof SignupSchema>
