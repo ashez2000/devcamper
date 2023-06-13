04a97b30d6a3520c68ba30bbffa5ec9f3edfb584
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-06-13 20:14:41 +0530

    Remove old files

--- src/review/review.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const CreateReviewSchema = z.object({
-  title: z.string().min(1).max(50),
-  text: z.string().min(1).max(500),
-  rating: z.number().min(1).max(5),
-});
-
-export type CreateReview = z.infer<typeof CreateReviewSchema>;
