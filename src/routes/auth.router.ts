d0bf0c184f1a22a90d28fc067dbb48cc2f6c0361
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-07-08 10:13:45 +0530

    Fix auth route imports

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 
-import { SignInInputSchema, SignUpInputSchema } from '../db/schema/user.schema'
-import { zodValidator } from '../middlewares/zod-validator.middleware'
-import * as authCtrl from '../controllers/auth.controller'
+import { createUserSchema, userCredentialSchema } from '$/schemas/user.schema'
+import { zodValidator } from '$/middlewares/zod-validator.middleware'
+import * as authCtrl from '$/controllers/auth.controller'
 
 const router = Router()
 
-router.post('/sign-up', zodValidator(SignUpInputSchema), authCtrl.signUp)
-router.post('/sign-in', zodValidator(SignInInputSchema), authCtrl.signIn)
+router.post('/sign-up', zodValidator(createUserSchema), authCtrl.signUp)
+router.post('/sign-in', zodValidator(userCredentialSchema), authCtrl.signIn)
 
 router.post('/forgot-password', authCtrl.forgotPassword)
 router.put('/reset-password', authCtrl.resetPassword)
