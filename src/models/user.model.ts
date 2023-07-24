0017540862fb7159e11b8b49fb90344d72a26244
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-07-24 15:43:39 +0530

    Add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose'
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'publisher', 'admin'],
+    default: 'user',
+  },
+})
+
+const User = model('User', UserSchema)
+
+export default User
