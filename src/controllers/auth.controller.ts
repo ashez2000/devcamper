ac1577a872f46d8da6950d3b8a05242a61e14b27
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-07-24 16:04:47 +0530

    Add user module

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,32 +1,31 @@
-import crypto from 'node:crypto'
 import * as R from 'ramda'
 import argon from 'argon2'
 import { Request, Response } from 'express'
 
-import { AppError } from '../utils/app-error.util'
-import { signToken } from '../libs/jwt'
-import { sendEmail } from '../services/email.service'
-import { CreateUser, UserCredential } from '../schemas/user.schema'
-import * as userRepo from '../repos/user.repo'
+import { AppError } from '$/utils/app-error.util'
+import { signToken } from '$/libs/jwt'
+import User from '$/models/user.model'
+import { UserRole } from '$/schemas/user.schema'
 
-const serializeUser = R.omit([
-  'password',
-  'resetPasswordToken',
-  'resetPasswordExpire',
-])
+const serializeUser = R.omit(['password'])
 
 // sign up
-export async function signUp(
-  req: Request<unknown, unknown, CreateUser>,
-  res: Response
-) {
-  const exist = await userRepo.findByEmail(req.body.email)
-  if (exist) {
-    throw new AppError('User already exists', 400)
-  }
+export async function signUp(req: Request, res: Response) {
+  const { name, email, password, role } = req.body
+  const hash = await argon.hash(password)
 
-  const user = await userRepo.create(req.body)
-  const token = signToken(user)
+  const user = await User.create({
+    name,
+    email,
+    password: hash,
+    role,
+  })
+
+  const token = signToken({
+    id: user._id.toString(),
+    email: user.email!,
+    role: user.role as UserRole,
+  })
 
   res.cookie('token', token, { httpOnly: true })
   res.status(200).json({
@@ -36,14 +35,20 @@ export async function signUp(
 }
 
 // sign in
-export async function signIn(
-  req: Request<unknown, unknown, UserCredential>,
-  res: Response
-) {
-  const user = await userRepo.findByCredential(req.body)
-  if (!user) throw new AppError('Invalid credential', 401)
+export async function signIn(req: Request, res: Response) {
+  const { email, password } = req.body
 
-  const token = signToken(user)
+  const user = await User.findOne({ email })
+  if (!user) throw new AppError('Invalid credentials', 401)
+
+  const isMatch = await argon.verify(user.password!, password)
+  if (!isMatch) throw new AppError('Invalid credentials', 401)
+
+  const token = signToken({
+    id: user._id.toString(),
+    email: user.email!,
+    role: user.role as UserRole,
+  })
 
   res.cookie('token', token, { httpOnly: true })
   res.status(200).json({
@@ -62,83 +67,8 @@ export async function signOut(_: Request, res: Response) {
 export async function currentUser(req: Request, res: Response) {
   if (!req.user) throw new AppError('Unauthorized', 401)
 
-  const user = await userRepo.findById(req.user.id)
+  const user = await User.findById(req.user.id)
   if (!user) throw new AppError('User not found', 404)
 
   res.status(200).json({ user: serializeUser(user) })
-}
-
-// forgot password
-export async function forgotPassword(req: Request, res: Response) {
-  const user = await userRepo.findByEmail(req.body.email)
-  if (!user) {
-    return res
-      .status(400)
-      .json({ message: 'No user with that email address exists' })
-  }
-
-  const resetPasswordToken = crypto.randomBytes(20).toString('hex')
-  const resetPasswordExpire = Date.now() + 10 * 60 * 1000
-  const hashedToken = await argon.hash(resetPasswordToken)
-
-  await userRepo.update(user.id, {
-    resetPasswordToken: hashedToken,
-    resetPasswordExpire: new Date(resetPasswordExpire),
-  })
-
-  const message = `Reset password token: ${resetPasswordToken}`
-
-  try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    })
-
-    res.status(200).json({
-      message: 'Email sent!',
-    })
-  } catch (err) {
-    await userRepo.update(user.id, {
-      resetPasswordToken: null,
-      resetPasswordExpire: null,
-    })
-
-    return res.status(500).json({
-      message: 'There was an error sending the email. Try again later!',
-    })
-  }
-}
-
-// reset password
-export async function resetPassword(req: Request, res: Response) {
-  const { resetToken, email, password } = req.body
-
-  const user = await userRepo.findByEmail(email)
-  if (!user) {
-    return res.status(400).json({ message: 'User does not exist' })
-  }
-
-  if (!user.resetPasswordToken || !user.resetPasswordExpire) {
-    return res.status(400).json({ message: 'Invalid reset token' })
-  }
-
-  const isMatch = await argon.verify(user.resetPasswordToken, resetToken)
-  if (!isMatch) {
-    return res.status(400).json({ message: 'Invalid reset token' })
-  }
-
-  if (user.resetPasswordExpire < new Date()) {
-    return res.status(400).json({ message: 'Reset token has expired' })
-  }
-
-  const hashedPassword = await argon.hash(password)
-
-  await userRepo.update(user.id, {
-    password: hashedPassword,
-    resetPasswordToken: null,
-    resetPasswordExpire: null,
-  })
-
-  res.status(200).json({ message: 'Password updated successfully' })
 }
--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,35 +1,11 @@
 import { Router } from 'express'
-
-import { createUserSchema, userCredentialSchema } from '../schemas/user.schema'
-import { zodValidator } from '../middlewares/zod-validator.middleware'
-import * as authCtrl from '../controllers/auth.controller'
+import * as authCtrl from '$/controllers/auth.controller'
 
 const router = Router()
 
-/**
- * @openapi
- * '/api/v1/auth/sign-up':
- *  post:
- *   tags:
- *   - Auth
- *   summary: Sign up a new user
- */
-router.post('/sign-up', zodValidator(createUserSchema), authCtrl.signUp)
-
-/**
- * @openapi
- * '/api/v1/auth/sign-in':
- *  post:
- *   tags:
- *   - Auth
- *   summary: Sign in a user
- */
-router.post('/sign-in', zodValidator(userCredentialSchema), authCtrl.signIn)
-
+router.post('/sign-up', authCtrl.signUp)
+router.post('/sign-in', authCtrl.signIn)
 router.post('/sign-out', authCtrl.signOut)
 router.get('/current-user', authCtrl.currentUser)
 
-router.post('/forgot-password', authCtrl.forgotPassword)
-router.put('/reset-password', authCtrl.resetPassword)
-
-export { router as authRouter }
+export default router
