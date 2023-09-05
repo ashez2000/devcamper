29da10cc2ce73fa582d1e43480811a0a3700b657
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-09-05 17:46:16 +0530

    Remove swagger docs and rate limiter

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,12 @@ import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
 import hpp from 'hpp'
-import swaggerUi from 'swagger-ui-express'
 
 import authRouter from '$/routes/auth.router'
 import bootcampRouter from '$/routes/bootcamp.router'
 // import courseRouter from '$/routes/course.router'
 // import reviewRouter from '$/routes/review.router'
 
-import apiSpec from '$/libs/swagger-doc'
-import limiter from '$/libs/rate-limit'
 import { globalError, notFound } from './error'
 
 const app = express()
@@ -22,7 +19,6 @@ const app = express()
 app.use(cors())
 app.use(helmet())
 app.use(hpp())
-app.use(limiter)
 
 app.use(express.json())
 app.use(cookieParser())
@@ -35,8 +31,6 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/bootcamps', bootcampRouter)
 // app.use('/api/v1/courses', courseRouter)
 // app.use('/api/v1/reviews', reviewRouter)
-
-app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(apiSpec))
 
 app.use(notFound)
 app.use(globalError)
