7f151cfa1c6c26e31381cbdc6490be80a7659ca4
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-09-09 21:17:19 +0530

    Add base api docs for bootcamps routes

--- src/routes/bootcamp.router.ts
+++ src/routes/bootcamp.router.ts
@@ -6,11 +6,49 @@ const router = Router()
 
 router.use('/:bootcampId/courses', courseRoutes)
 
+/**
+ * @openapi
+ * /api/v1/bootcamps:
+ *  get:
+ *   tags:
+ *   - Bootcamp
+ */
 router.get('/', bootcampCtrl.getBootcamps)
+
+/**
+ * @openapi
+ * /api/v1/bootcamps:
+ *  post:
+ *   tags:
+ *   - Bootcamp
+ */
 router.post('/', bootcampCtrl.createBootcamp)
 
+/**
+ * @openapi
+ * /api/v1/bootcamps/{bootcampId}:
+ *  get:
+ *   tags:
+ *   - Bootcamp
+ */
 router.get('/:id', bootcampCtrl.getBootcamp)
+
+/**
+ * @openapi
+ * /api/v1/bootcamps/{bootcampId}:
+ *  put:
+ *   tags:
+ *   - Bootcamp
+ */
 router.put('/:id', bootcampCtrl.updateBootcamp)
+
+/**
+ * @openapi
+ * /api/v1/bootcamps/{bootcampId}:
+ *  delete:
+ *   tags:
+ *   - Bootcamp
+ */
 router.delete('/:id', bootcampCtrl.deleteBootcamp)
 
 export default router
