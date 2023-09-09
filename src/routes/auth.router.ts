3ac83a044f9bba4daebf4d9020e630b04fa24025
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-09-09 21:07:02 +0530

    Add base api docs for auth routes

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -3,12 +3,58 @@ import * as authCtrl from '@/controllers/auth.controller'
 
 const router = Router()
 
+/**
+ * @openapi
+ * /api/v1/auth/signup:
+ *  post:
+ *   tags:
+ *   - Auth
+ */
 router.post('/signup', authCtrl.signup)
+
+/**
+ * @openapi
+ * /api/v1/auth/signin:
+ *  post:
+ *   tags:
+ *   - Auth
+ */
 router.post('/signin', authCtrl.signin)
+
+/**
+ * @openapi
+ * /api/v1/auth/signout:
+ *  post:
+ *   tags:
+ *   - Auth
+ */
 router.post('/signout', authCtrl.signout)
+
+/**
+ * @openapi
+ * /api/v1/auth/profile:
+ *  post:
+ *   tags:
+ *   - Auth
+ */
 router.get('/profile', authCtrl.profile)
 
+/**
+ * @openapi
+ * /api/v1/auth/forgot-password:
+ *  post:
+ *   tags:
+ *   - Auth
+ */
 router.post('/forgot-password', authCtrl.forgotPassword)
+
+/**
+ * @openapi
+ * /api/v1/auth/reset-password/{resetToken}:
+ *  put:
+ *   tags:
+ *   - Auth
+ */
 router.put('/reset-password/:resetToken', authCtrl.resetPassword)
 
 export default router
