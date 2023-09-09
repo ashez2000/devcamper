d668cdf8901e48a1dd8e66cc131e0d4ec7b71c17
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-09-09 21:29:21 +0530

    Add base api docs for course and review routes

--- src/routes/review.router.ts
+++ src/routes/review.router.ts
@@ -1,30 +1,49 @@
 import { Router } from 'express'
-
-import { authenticate, authorize } from '$/middlewares/auth.middleware'
 import * as reviewCtrl from '$/controllers/review.controller'
 
 const router = Router({ mergeParams: true })
 
+/**
+ * @openapi
+ * /api/v1/bootcamps/{bootcampId}/reviews:
+ *  get:
+ *   tags:
+ *   - Review
+ */
+/**
+ * @openapi
+ * /api/v1/reviews:
+ *  get:
+ *   tags:
+ *   - Review
+ */
 router.get('/', reviewCtrl.getReviews)
-router.post(
-  '/',
-  authenticate,
-  authorize('user', 'admin'),
-  reviewCtrl.createReview
-)
 
-router.put(
-  '/:id',
-  authenticate,
-  authorize('user', 'admin'),
-  reviewCtrl.updateReview
-)
+/**
+ * @openapi
+ * /api/v1/bootcamps/{bootcampId}/reviews:
+ *  post:
+ *   tags:
+ *   - Review
+ */
+router.post('/', reviewCtrl.createReview)
 
-router.delete(
-  '/:id',
-  authenticate,
-  authorize('user', 'admin'),
-  reviewCtrl.deleteReview
-)
+/**
+ * @openapi
+ * /api/v1/reviews/{reviewId}:
+ *  put:
+ *   tags:
+ *   - Review
+ */
+router.put('/:id', reviewCtrl.updateReview)
+
+/**
+ * @openapi
+ * /api/v1/reviews/{reviewId}:
+ *  delete:
+ *   tags:
+ *   - Review
+ */
+router.delete('/:id', reviewCtrl.deleteReview)
 
 export default router
