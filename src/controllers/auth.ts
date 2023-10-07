4502c2b774e1238a8127d4d48d79e7e103e36c71
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-10-07 19:19:38 +0530

    Refactor signin

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -6,3 +6,9 @@ export async function signup(req: Request, res: Response) {
   const data = await userSrv.createUser(req.body)
   res.cookie('token', data.token).status(201).json(data)
 }
+
+// path: POST /api/{ver}/auth/signin | public
+export async function signin(req: Request, res: Response) {
+  const data = await userSrv.findUserByCredentials(req.body)
+  res.cookie('token', data.token).status(201).json(data)
+}
--- src/services/users.ts
+++ src/services/users.ts
@@ -1,9 +1,23 @@
 import argon from 'argon2'
 import { omit, pick } from 'ramda'
+import { User } from '@prisma/client'
 
 import db from '@/utils/prisma'
+import { AppError } from '@/utils/app-error'
 import { signToken } from '@/utils/jwt'
-import { SignupSchema } from '@/schemas/user-schema'
+import { SignupSchema, SigninSchema } from '@/schemas/user-schema'
+
+const UserNotFoundError = () => new AppError('User not found!', 404)
+const InvalidCredentialsError = () => new AppError('Invalid credentials', 404)
+
+function tokenResponse(user: User) {
+  const token = signToken(pick(['id', 'email', 'role'], user))
+
+  return {
+    user: omit(['password'], user),
+    token: token,
+  }
+}
 
 export async function createUser(data: any) {
   const { name, email, password, role } = SignupSchema.parse(data)
@@ -19,10 +33,20 @@ export async function createUser(data: any) {
     },
   })
 
-  const token = signToken(pick(['id', 'email', 'role'], user))
+  return tokenResponse(user)
+}
 
-  return {
-    user: omit(['password'], user),
-    token: token,
+export async function findUserByCredentials(data: any) {
+  const { email, password } = SigninSchema.parse(data)
+  const user = await db.user.findUnique({ where: { email } })
+  if (!user) {
+    throw InvalidCredentialsError()
   }
+
+  const isMatch = await argon.verify(user.password, password)
+  if (!isMatch) {
+    throw InvalidCredentialsError()
+  }
+
+  return tokenResponse(user)
 }
