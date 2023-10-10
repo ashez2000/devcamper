ec93a1fb67d59c0250f48dcc3eb4afdfe33fae1e
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-10-10 17:31:27 +0530

    Add eslint

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import logger from '@/utils/logger'
 
 async function main() {
   try {
-    let server = http.createServer(app)
+    const server = http.createServer(app)
     server.listen(PORT, () => {
       logger.info(`main: Listening { port: ${PORT}, env: ${NODE_ENV} }`)
     })
 
-    function shutdown() {
+    const shutdown = () => {
       server.close()
     }
 
