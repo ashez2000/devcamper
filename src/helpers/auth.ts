ac28050e260a850cf8a85927cb80bb8682f2f6d9
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-10-14 18:04:16 +0530

    fix: auth helper vars

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -5,12 +5,12 @@ import { verifyToken } from '@/utils/jwt'
 import { UnauthorizedError } from '@/utils/app-error'
 
 export function getAuthPayload(req: Request, restrict?: Role[]) {
-  let token = req.cookies.token
+  const token = req.cookies.token
   if (!token) {
     throw UnauthorizedError()
   }
 
-  let payload = verifyToken(token)
+  const payload = verifyToken(token)
   if (!payload) {
     throw UnauthorizedError()
   }
