f02ec18ee3001ae2b85afcf37c016de74a9abeb9
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-10-14 16:17:40 +0530

    Remove old utils

--- src/app.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest'
-import supertest from 'supertest'
-import { app } from './app'
-
-test('GET /health', async () => {
-  const res = await supertest(app).get('/health')
-  expect(res.status).toBe(200)
-})
